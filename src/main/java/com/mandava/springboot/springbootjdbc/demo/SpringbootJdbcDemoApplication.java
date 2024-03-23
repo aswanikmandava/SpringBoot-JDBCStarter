dffd8f34908327646505a471cea46ba0ae9c7713
aswanikmandava <aswani.k.mandava@gmail.com>
2024-03-23 16:41:10 -0400

    Initial draft version

---
+++ src/main/java/com/mandava/springboot/springbootjdbc/demo/SpringbootJdbcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mandava.springboot.springbootjdbc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJdbcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJdbcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mandava/springboot/springbootjdbc/demo/SpringbootJdbcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandava.springboot.springbootjdbc.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJdbcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
