b5632b7eaff8de2858820d55bce2c48662b48266
aswanikmandava <aswani.k.mandava@gmail.com>
2024-03-24 17:26:02 -0400

    Added examples to insert, update and delete player

--- src/main/java/com/mandava/springboot/springbootjdbc/demo/PlayerDao.java
+++ src/main/java/com/mandava/springboot/springbootjdbc/demo/PlayerDao.java
@@ -1,5 +1,6 @@
 package com.mandava.springboot.springbootjdbc.demo;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,5 +16,29 @@ public class PlayerDao {
 	public List<Player> getAllPlayers(){
 		String sql = "SELECT * FROM Player";
 		return jdbcTemplate.query(sql, new BeanPropertyRowMapper<Player>(Player.class));
+	}
+	
+	public Player getPlayerById(int id) {
+		String sql = "SELECT * FROM Player where ID = ?";
+		return jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<Player>(Player.class), new Object[] {id});
+	}
+	
+	public int insertPlayer(Player player) {
+		String sql = "INSERT INTO Player(ID, Name, Nationality, Birth_date, Titles) VALUES(?, ?, ?, ?, ?)";
+		return jdbcTemplate.update(sql, new Object[] {player.getId(), player.getName(), player.getNationality(),
+				new Timestamp(player.getBirthDate().getTime()), player.getTitles()});
+	}
+	
+	public int updatePlayer(Player player) {
+		String sql = "UPDATE Player SET Name = ?, Nationality = ?, Birth_date = ?, Titles = ? " +
+					"WHERE ID = ?";
+		return jdbcTemplate.update(sql, new Object[] {player.getName(), player.getNationality(),
+				new Timestamp(player.getBirthDate().getTime()), player.getTitles(), player.getId()});
+		
+	}
+	
+	public int deletePlayerById(int id){
+	    String sql="DELETE FROM PLAYER WHERE ID = ?";
+	    return jdbcTemplate.update(sql, new Object[] {id});
 	}
 }
--- src/main/java/com/mandava/springboot/springbootjdbc/demo/TennisPlayerApplication.java
+++ src/main/java/com/mandava/springboot/springbootjdbc/demo/TennisPlayerApplication.java
@@ -2,6 +2,9 @@ package com.mandava.springboot.springbootjdbc.demo;
 
 
 import org.slf4j.LoggerFactory;
+
+import java.sql.Date;
+
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,6 +25,10 @@ public class TennisPlayerApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
+		// logger.info("Getting player by id(3): {}", dao.getPlayerById(3));
+		// logger.info("Inserting a new player(4): {}", dao.insertPlayer(new Player(4, "Nadal", "Serbia", new Date(System.currentTimeMillis()), 10)));
+		// logger.info("Updating player with id 4: ", dao.updatePlayer(new Player(4, "Nadal", "Russia", Date.valueOf("2000-01-15"), 15)));
+		logger.info("Deleting player with id 4:", dao.deletePlayerById(4));
 		logger.info("All Players Data: {}", dao.getAllPlayers());
 	}
 
